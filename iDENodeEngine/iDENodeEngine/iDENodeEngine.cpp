dbd455777cbef6e833122122cbb648dff3dd9297
Dora Lee <dsa28s@naver.com>
2021-07-21 23:31:54 +0900

    chore(nodejs-engine): iDENodeEngine project setup

---
+++ iDENodeEngine/iDENodeEngine/iDENodeEngine.cpp
@@ -0,0 +1,10 @@
+
+#import "iDENodeEngine.h"
+
+namespace node {
+    int Start(int argc, char *argv[]);
+} // namespace node
+
+int node_start(int argc, char *argv[]) {
+    return node::Start(argc, argv);
+}
