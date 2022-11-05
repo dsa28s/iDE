e710008ff4a67bc879582454c48a19d5e501f707
Dora Lee <dsa28s@naver.com>
2022-11-05 23:27:15 +0900

    chore: iDE iPadOS Project setup

---
+++ iDE/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iDE
+//
+//  Created by 도라도라 on 2022/11/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDE/iDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDEApp.swift
+//  iDE
+//
+//  Created by 도라도라 on 2022/11/05.
+//
+
+import SwiftUI
+
+@main
+struct iDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
