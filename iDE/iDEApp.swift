4fc0e182f8fd4e1682613952fc988dc5abd9d3dc
Dora Lee <dsa28s@naver.com>
2022-11-08 16:55:34 +0900

    feat: add jit check logic

--- iDE/iDEApp.swift
+++ iDE/iDEApp.swift
@@ -1,13 +1,34 @@
-//
-//  iDEApp.swift
-//  iDE
-//
-//  Created by 도라도라 on 2022/11/05.
-//
-
 import SwiftUI
 
+
 @main
+class iDEMain {
+    static var jitAvailable = true
+    
+    static func main() {
+        if jb_spawn_ptrace_child(CommandLine.argc, CommandLine.unsafeArgv) {
+            logger.info("JIT: ptrace() child spawn trick")
+        } else if jb_has_jit_entitlement() {
+            logger.info("JIT: found entitlement")
+        } else if jb_has_cs_disabled() {
+            logger.info("JIT: CS_KILL disabled")
+        } else if jb_has_cs_execseg_allow_unsigned() {
+            logger.info("JIT: CS_EXECSEG_ALLOW_UNSIGNED set")
+        } else if jb_enable_ptrace_hack() {
+            logger.info("JIT: ptrace() hack supported")
+        } else {
+            logger.info("JIT: ptrace() hack failed")
+            jitAvailable = false
+        }
+        // raise memlimits on jailbroken devices
+        if jb_increase_memlimit() {
+            logger.info("MEM: successfully removed memory limits")
+        }
+        
+        iDEApp.main()
+    }
+}
+
 struct iDEApp: App {
     var body: some Scene {
         WindowGroup {
