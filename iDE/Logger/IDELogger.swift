b1bc3355badfec68dc98d812cedd5a623115f637
Dora Lee <dsa28s@naver.com>
2022-11-09 19:09:00 +0900

    chore: fix file header

--- iDE/Logger/IDELogger.swift
+++ iDE/Logger/IDELogger.swift
@@ -2,18 +2,17 @@
 //
 // iDE Project
 // File Name : IDELogger.swift
-// Description : JIT Manager for IDE (original source from UTM)
+// Description : Global logger for iDE
 // Author: Dora Lee <lee@sanghun.io>
 
 import Logging
 
-
 let logger = Logger(label: "io.sanghun.IDE") { label in
     var ideLogger = IDELoggingHandler(label: label)
     var stdOutLogger = StreamLogHandler.standardOutput(label: label)
-    
+
     ideLogger.logLevel = .debug
     stdOutLogger.logLevel = .debug
-    
+
     return MultiplexLogHandler([ideLogger, stdOutLogger])
 }
