e91b444068cadfcc0d2a9fcb22594c49c46cd8e0
Dora Lee <dsa28s@naver.com>
2022-11-15 02:29:37 +0900

    feat: add app global TCA

--- iDE/MainEntryPoint.swift
+++ iDE/MainEntryPoint.swift
@@ -9,36 +9,76 @@ import ComposableArchitecture
 import SheeKit
 import SwiftUI
 
+enum ScreenType {
+    case licenseAgreement
+    case jitCheck
+    case engineInitialize
+}
+
 struct ContentView: View {
-    let licenseAgreementStore: StoreOf<LicenseAgreement>
+    let store: StoreOf<IDE>
 
     var body: some View {
-        WithViewStore(self.licenseAgreementStore, observe: { $0 }) { viewStore in
-            ZStack {
-                ProgressView()
-                    .progressViewStyle(.circular)
-                    .controlSize(.large)
-            }.shee(
-                isPresented: !viewStore.binding(get: \.isLicenseAgreed, send: .agreeLicense),
-                presentationStyle: .formSheet(
-                    properties: .init()
-                ),
-                presentedViewControllerParameters: presentedLicenseAgreementSheetParams
-            ) {
-                IDELicenseAgreementDialogView(
-                    onAgreed: { viewStore.send(.agreeLicense) },
-                    onDisagreed: { viewStore.send(.disagreeLicense) }
-                )
+        SwitchStore(self.store) {
+            CaseLet(state: /IDE.State.licenseAgreement, action: IDE.Action.licenseAgreement) { store in
+                WithViewStore(store, observe: { $0 }) { viewStore in
+                    let isLicenseAgreed = viewStore.binding(get: \.isLicenseAgreed, send: .agreeLicense)
+
+                    ZStack {
+                        ProgressView()
+                            .progressViewStyle(.circular)
+                            .controlSize(.large)
+                    }.shee(
+                        isPresented: !isLicenseAgreed,
+                        presentationStyle: .formSheet(
+                            properties: .init()
+                        ),
+                        presentedViewControllerParameters: createSheetParams(width: 700, height: 600)
+                    ) {
+                        IDELicenseAgreementDialogView(
+                            onAgreed: { viewStore.send(.agreeLicense) },
+                            onDisagreed: { viewStore.send(.disagreeLicense) }
+                        )
+                    }.onAppear {
+                        if viewStore.isLicenseAgreed {
+                            viewStore.send(.agreeLicense)
+                        }
+                    }
+                }
+            }
+            CaseLet(state: /IDE.State.jitChecker, action: IDE.Action.jitChecker) { store in
+                WithViewStore(store, observe: { $0 }) { viewStore in
+                    let jitAvailable = viewStore.binding(get: \.isJITAvailable, send: .unavailable)
+
+                    ZStack {
+                        ProgressView()
+                            .progressViewStyle(.circular)
+                            .controlSize(.large)
+                    }.shee(
+                        isPresented: !jitAvailable,
+                        presentationStyle: .formSheet(
+                            properties: .init()
+                        ),
+                        presentedViewControllerParameters: createSheetParams(width: 700, height: 365)
+                    ) {
+                        IDEJITAvailableDialogView(onConfirmed: { viewStore.send(.unavailable) })
+                    }.onAppear {
+                        if viewStore.isJITAvailable {
+                            print("AVAILABLE")
+                            viewStore.send(.available)
+                        }
+                    }
+                }
             }
         }
     }
 
-    var presentedLicenseAgreementSheetParams: UIViewControllerProxy {
+    private func createSheetParams(width: Int, height: Int) -> UIViewControllerProxy {
         var parameters = UIViewControllerProxy()
         parameters.preferredStatusBarUpdateAnimation = .fade
         parameters.isModalInPresentation = true
         parameters.modalTransitionStyle = .coverVertical
-        parameters.preferredContentSize = .init(width: 700, height: 600)
+        parameters.preferredContentSize = .init(width: width, height: height)
         return parameters
     }
 }
--- iDE/iDEApp.swift
+++ iDE/iDEApp.swift
@@ -1,8 +1,57 @@
+// Copyright (c) 2022 Dora Lee
+//
+// iDE Project
+// File Name : iDEApp.swift
+// Description : iDEApp Config / TCA Logic
+// Author: Dora Lee <lee@sanghun.io>
+
 import ComposableArchitecture
 import SwiftUI
 
+struct IDE: ReducerProtocol {
+    enum State: Equatable {
+        case licenseAgreement(IDELicenseAgreement.State)
+        case jitChecker(IDEJITChecker.State)
+
+        init() {
+            self = .licenseAgreement(IDELicenseAgreement.State())
+        }
+    }
+
+    enum Action: Equatable {
+        case licenseAgreement(IDELicenseAgreement.Action)
+        case jitChecker(IDEJITChecker.Action)
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .licenseAgreement(.agreeLicense):
+                state = .jitChecker(IDEJITChecker.State())
+                return .none
+
+            case .licenseAgreement:
+                return .none
+
+            case .jitChecker(.available):
+                // TODO: VM 프로비저닝 화면으로 넘기기
+                return .none
+
+            case .jitChecker:
+                return .none
+            }
+        }
+        .ifCaseLet(/State.licenseAgreement, action: /Action.licenseAgreement) {
+            IDELicenseAgreement()
+        }
+        .ifCaseLet(/State.jitChecker, action: /Action.jitChecker) {
+            IDEJITChecker()
+        }
+    }
+}
+
 @main
-class iDEMain {
+enum iDEMain {
     static var jitAvailable = true
 
     static func main() {
@@ -33,7 +82,10 @@ struct iDEApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(
-                licenseAgreementStore: Store(initialState: LicenseAgreement.State(), reducer: LicenseAgreement())
+                store: Store(
+                    initialState: IDE.State(),
+                    reducer: IDE()
+                )
             )
         }
     }
