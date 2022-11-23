61657ae1e11c37c8d1940980db7ffd5ee5c1d893
Dora Lee <dsa28s@naver.com>
2022-11-23 17:42:33 +0900

    feat: Add VM File Checker

---
+++ iDE/Core/VMFileChecker.swift
@@ -0,0 +1,60 @@
+// Copyright (c) 2022 Dora Lee
+//
+// iDE Project
+// File Name : VMFileChecker.swift
+// Description : iDE VM Image Checker
+// Author: Dora Lee <lee@sanghun.io>
+
+
+import Foundation
+import ComposableArchitecture
+import XCTestDynamicOverlay
+
+
+struct IDEVmFileCheckerResult: Equatable, Sendable {
+    var alreadyInstalled: Bool
+}
+
+struct IDEVmFileChecker: Sendable {
+    var check: @Sendable () async -> IDEVmFileCheckerResult
+}
+
+extension IDEVmFileChecker: DependencyKey {
+    static let liveValue = Self(
+        check: {
+            let paths = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)
+            let libraryDirectory = paths[0]
+            let libraryUrl = URL(string: libraryDirectory)!
+            let vmPath = libraryUrl.appendingPathComponent("VM")
+            
+            if !FileManager.default.fileExists(atPath: vmPath.absoluteString) {
+                do {
+                    try FileManager.default.createDirectory(atPath: vmPath.absoluteString, withIntermediateDirectories: true, attributes: nil)
+                } catch {
+                    logger.info("\(error.localizedDescription)")
+                }
+            }
+            
+            let fdPath = vmPath.appendingPathComponent("efi_vars.fd")
+            let imagePath = vmPath.appendingPathComponent("57B22AF2-F4B6-4A72-9C72-16E9BDECFE91.qcow2")
+            
+            let fdExist = FileManager.default.fileExists(atPath: fdPath.absoluteString)
+            let imageExist = FileManager.default.fileExists(atPath: imagePath.absoluteString)
+            
+            return IDEVmFileCheckerResult(alreadyInstalled: fdExist && imageExist)
+        }
+    )
+}
+
+extension IDEVmFileChecker: TestDependencyKey {
+    static let testValue = Self(
+        check: unimplemented("\(Self.self).check")
+    )
+}
+
+extension DependencyValues {
+    var ideVmFileChecker: IDEVmFileChecker {
+        get { self[IDEVmFileChecker.self] }
+        set { self[IDEVmFileChecker.self] = newValue }
+    }
+}
